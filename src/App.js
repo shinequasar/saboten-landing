56cb97c03c49a861d59cea7ecf9968b7caccf1ff
shinequasar <shinequasar@gmail.com>
2023-05-03 00:56:49 +0900

    [feat]테마설정

--- src/App.js
+++ src/App.js
@@ -8,20 +8,36 @@ import 'aos/dist/aos.css';
 import CategoryList from './componets/CategoryList'
 import Downloads from "./componets/Downloads";
 import Makers from "./componets/Makers";
+import { ThemeProvider } from 'styled-components';
 
 const App = () => {
   AOS.init();
+  const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const theme = prefersDarkMode ? darkTheme : lightTheme;
+  
   return (
     <div className="App">
-        <Title/>
-        <Introduce/>
-        <Intro/>
-        <CategoryList/>
-        <Downloads/>
-        <Makers/>
+       <ThemeProvider theme={theme}>
+          <Title/>
+          <Introduce/>
+          <Intro/>
+          <CategoryList/>
+          <Downloads/>
+          <Makers/>
+        </ThemeProvider>
     </div>
   );
 }
 
 export default App;
 
+
+const lightTheme = {
+  body: '#FFFFFF',
+  text: '#000000',
+};
+
+const darkTheme = {
+  body: '#1C1C1E',
+  text: '#FFFFFF',
+};
